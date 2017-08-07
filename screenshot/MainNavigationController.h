a8bbba9dfee66b0b77192bc4462916fac921d5cb
Corey Werner <cnotethegr8@cnote.local>
2017-08-07 16:03:07 +0300

    init

---
+++ screenshot/MainNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  MainNavigationController.h
+//  screenshot
+//
+//  Created by Corey Werner on 8/7/17.
+//  Copyright © 2017 crazeapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainNavigationController : UINavigationController
+
+@end
