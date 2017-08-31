780aa39650d40bb6747fff3b88b4babbb5171e0f
Corey Werner <cnotethegr8@cnote.local>
2017-08-31 15:33:01 +0300

    create delay from tutorial screenshot to screenshot vc

--- screenshot/AppDelegate.m
+++ screenshot/AppDelegate.m
@@ -194,7 +194,11 @@
     viewController.delegate = nil;
     
     [self prepareDataStackCompletionIfNeeded];
-    [self transitionToViewController:[self nextViewController]];
+    
+    // Create a delay for a more natural feel after taking the screentshot
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self transitionToViewController:[self nextViewController]];
+    });
 }
 
 
