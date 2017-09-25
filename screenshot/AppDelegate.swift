1dd8ef239da84a3c837683d624d7f90bf8ce1f67
Gershon Kagan <gershon@glidetalk.com>
2017-09-25 15:08:51 +0300

    Add sessionStarted / sessionEnded.

--- screenshot/AppDelegate.swift
+++ screenshot/AppDelegate.swift
@@ -51,15 +51,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        AnalyticsManager.track("sessionEnded")
         bgTask = application.beginBackgroundTask(withName: "liveAsLongAsCan") { // TODO: Die before killed by system?
             application.endBackgroundTask(self.bgTask)
             self.bgTask = UIBackgroundTaskInvalid
         }
     }
 
-//    func applicationWillEnterForeground(_ application: UIApplication) {
-//        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-//    }
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        AnalyticsManager.track("sessionStarted")
+    }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
