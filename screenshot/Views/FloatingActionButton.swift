e666d82fca6130f5b7016fe42a48c8819ef62811
Corey Werner <cnotethegr8@cnote.local>
2017-09-27 12:32:23 +0300

    included shadow to fab

--- screenshot/Views/FloatingActionButton.swift
+++ screenshot/Views/FloatingActionButton.swift
@@ -9,9 +9,23 @@
 import Foundation
 
 class FloatingActionButton: UIButton {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 2)
+        layer.shadowRadius = 3
+        layer.shadowOpacity = 0.5
+    }
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         
         layer.cornerRadius = bounds.size.width * 0.5
+        layer.shadowPath = UIBezierPath(ovalIn: bounds).cgPath
     }
 }
