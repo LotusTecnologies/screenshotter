115e7e331fdfffa47ab70321527c677e82f009ff
Jacob Relkin <jacobrelkin@gmail.com>
2017-10-03 10:48:49 +0300

    Refactored done button creation in overlay controller

--- screenshot/Pages/Tutorial/TutorialVideoOverlayViewController.swift
+++ screenshot/Pages/Tutorial/TutorialVideoOverlayViewController.swift
@@ -9,13 +9,37 @@
 import UIKit
 
 class TutorialVideoOverlayViewController : UIViewController {
-    var doneButtonTapped: (() -> Void)?
+    private var doneButton: UIButton!
     
+    // Closure to be executed when the done button is tapped
+    var doneButtonTapped: (() -> Void)?
+
+    // MARK: - UIViewController
     override func viewDidLoad() {
-        let doneButton = UIButton(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        setupDoneButton()
+        setupDoneButtonConstraints()
+    }
+    
+    // MARK: - Actions
+    
+    @objc func didTapOnDoneButton() {
+        doneButtonTapped?()
+    }
+    
+    // MARK - Private
+    
+    private func setupDoneButtonConstraints() {
+        doneButton.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        doneButton.heightAnchor.constraint(equalToConstant: 35).isActive = true
+        doneButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
+        doneButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 25).isActive = true
+    }
+    
+    private func setupDoneButton() {
+        doneButton = UIButton(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
         doneButton.layer.borderColor = UIColor.white.cgColor
         doneButton.layer.borderWidth = 2
-
+        
         if #available(iOS 11.0, *) {
             doneButton.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMinYCorner]
         } else {
@@ -26,20 +50,8 @@ class TutorialVideoOverlayViewController : UIViewController {
         doneButton.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
         doneButton.setTitle("Done", for: .normal)
         doneButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(doneButton)
-        
-        doneButton.widthAnchor.constraint(equalToConstant: 70).isActive = true
-        doneButton.heightAnchor.constraint(equalToConstant: 35).isActive = true
-        doneButton.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
-        doneButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 25).isActive = true
-     
         doneButton.addTarget(self, action: #selector(didTapOnDoneButton), for: .touchUpInside)
         
-        view.setNeedsLayout()
-    }
-    
-    @objc func didTapOnDoneButton() {
-        doneButtonTapped?()
+        view.addSubview(doneButton)
     }
 }
