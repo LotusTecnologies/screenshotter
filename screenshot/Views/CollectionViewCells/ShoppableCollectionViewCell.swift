e98b433012496c329fa82063a8a16a5f9f7a8c9a
Corey Werner <cnotethegr8@cnote.local>
2017-11-15 15:45:29 +0200

    converted shoppable cell to swift

---
+++ screenshot/Views/CollectionViewCells/ShoppableCollectionViewCell.swift
@@ -0,0 +1,51 @@
+//
+//  ShoppableCollectionViewCell.swift
+//  screenshot
+//
+//  Created by Corey Werner on 11/15/17.
+//  Copyright © 2017 crazeapp. All rights reserved.
+//
+
+import Foundation
+
+class ShoppableCollectionViewCell : UICollectionViewCell {
+    private let imageView = UIImageView()
+    private let borderColor = UIColor.gray8
+    
+    var image: UIImage? {
+        set {
+            imageView.image = newValue
+        }
+        get {
+            return imageView.image
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        contentView.backgroundColor = .white
+        layer.borderColor = borderColor.cgColor
+        layer.borderWidth = 1.5
+        layer.cornerRadius = .defaultCornerRadius
+        layer.masksToBounds = true
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        contentView.addSubview(imageView)
+        imageView.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor).isActive = true
+        imageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor).isActive = true
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            layer.borderColor = isSelected ? UIColor.crazeRed.cgColor : borderColor.cgColor;
+        }
+    }
+}
