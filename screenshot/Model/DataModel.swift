2e8666e2ca38948d92559711c915dded96ba2f7d
Gershon Kagan <gershon@glidetalk.com>
2017-11-19 12:43:31 +0200

    Consistent listing of shoppables across devices.

--- screenshot/Model/DataModel.swift
+++ screenshot/Model/DataModel.swift
@@ -117,7 +117,7 @@ class DataModel: NSObject {
         hasShoppablesFrc?.delegate = self
         
         let request: NSFetchRequest<Shoppable> = Shoppable.fetchRequest()
-        request.sortDescriptors = [NSSortDescriptor(key: "order", ascending: true), NSSortDescriptor(key: "offersURL", ascending: true)]
+        request.sortDescriptors = [NSSortDescriptor(key: "order", ascending: true), NSSortDescriptor(key: "b0x", ascending: true), NSSortDescriptor(key: "b0y", ascending: true), NSSortDescriptor(key: "b1x", ascending: true), NSSortDescriptor(key: "b1y", ascending: true), NSSortDescriptor(key: "offersURL", ascending: true)]
         request.predicate = NSPredicate(format: "screenshot == %@ AND productCount > 0", screenshot)
         let fetchedResultsController = ShoppableFrc(fetchRequest: request, managedObjectContext: self.mainMoc(), sectionNameKeyPath: nil, cacheName: nil, hasShoppablesFrc: hasShoppablesFrc!)
         shoppableFrc = fetchedResultsController as NSFetchedResultsController<Shoppable>
