35c50b90ff69fc4e61b9dc509636fb3e48114424
Jacob Relkin <jacobrelkin@gmail.com>
2017-12-18 11:03:33 +0200

    Move ChangelogResponse and the changelog networking function into the ChangelogAlert.swift file

--- screenshot/Controllers/ChangelogAlert.swift
+++ screenshot/Controllers/ChangelogAlert.swift
@@ -9,6 +9,46 @@
 import UIKit
 import PromiseKit
 
+struct ChangelogResponse : Decodable {
+    enum CodingKeys : String, CodingKey {
+        case title = "title"
+        case body = "body"
+    }
+    
+    var title: String? = nil
+    let body: String
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        do {
+            title = try container.decode(String.self, forKey: .title)
+        } catch {}
+        
+        body = try container.decode(String.self, forKey: .body)
+    }
+}
+
+
+extension NetworkingPromise {
+    static func changelog(forAppVersion appVersion: String, localeIdentifier: String) -> Promise<ChangelogResponse> {
+        let urlString = [Constants.whatsNewDomain, appVersion, "\(localeIdentifier).json"].joined(separator: "/")
+        guard let URL = URL(string: urlString) else {
+            return Promise(error: NSError(domain: "Craze", code: 0, userInfo: [NSLocalizedDescriptionKey : "Unable to construct changelog URL"]))
+        }
+        
+        let request = URLRequest(url: URL, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 30)
+        let promise:URLDataPromise = URLSession.shared.dataTask(with: request)
+        return promise.asDataAndResponse().then { data, response -> Promise<ChangelogResponse> in
+            guard let response = response as? HTTPURLResponse, response.statusCode >= 200 && response.statusCode < 300 else {
+                return Promise(error: NSError(domain: "Craze", code: 0, userInfo: [NSLocalizedDescriptionKey : "Invalid status code received from AWS lambda"]))
+            }
+            
+            return Promise(value: try JSONDecoder().decode(ChangelogResponse.self, from: data))
+        }
+    }
+}
+
 struct ChangelogAlert {
     static var lastVersion: String? {
         get {
@@ -43,8 +83,9 @@ struct ChangelogAlert {
                 print(error)
             }
             .then(on: .main) { response in
-                let controller = UIAlertController(title: response.title, message: response.body, preferredStyle: .alert)
-                controller.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                let title = response.title ?? "changelog.alert.title.default".localized
+                let controller = UIAlertController(title: title, message: response.body, preferredStyle: .alert)
+                controller.addAction(UIAlertAction(title: "generic.ok".localized, style: .default, handler: nil))
                 viewController.present(controller, animated: true, completion: nil)
                 
                 return AnyPromise(Promise<Void>())
