9059d752671c71c15e4b7bdd48905ee03893ce09
Corey Werner <cnotethegr8@cnote.local>
2017-12-20 07:35:57 +0200

    completed tappable text class

--- screenshot/View/Generic/TextView/TappableTextView.swift
+++ screenshot/View/Generic/TextView/TappableTextView.swift
@@ -8,6 +8,73 @@
 
 import Foundation
 
-class _TappableTextView : UITextView {
+class _TappableTextView : UITextView, TappableTextProtocol {
+    weak var tappableTextDelegate: TappableTextDelegate?
+    private var tappableIndexes: [UInt]?
     
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        
+        addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapGestureRecognizerAction(_:))))
+    }
+    
+    @objc private func tapGestureRecognizerAction(_ tapGesture: UITapGestureRecognizer) {
+        guard let tappableIndexes = tappableIndexes, let tappableTextDelegate = tappableTextDelegate else {
+            return
+        }
+        
+        var location = tapGesture.location(in: self)
+        location.x -= textContainerInset.left
+        location.y -= textContainerInset.top
+        
+        let characterIndex = layoutManager.characterIndex(for: location, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+        
+        if textStorage.length > characterIndex {
+            for tappableIndex in tappableIndexes {
+                if attributedText.attribute("\(tappableIndex)", at: characterIndex, effectiveRange: nil) != nil {
+                    tappableTextDelegate.tappableText(view: self, tappedTextAt: tappableIndex)
+                    break
+                }
+            }
+        }
+    }
+    
+    func applyTappableText(_ texts: [[String : UInt]], with attributes: [String : AnyObject]? = nil) {
+        var tappableIndexes: [UInt] = []
+        let attributedString = NSMutableAttributedString()
+        
+        for i in 0 ..< texts.count {
+            let dictionary = texts[i]
+            
+            guard let text = dictionary.keys.first else {
+                continue
+            }
+            
+            let isTappable = dictionary.values.first ?? 0 > 0
+            var fragmentAttributes: [String : Any]?
+            
+            if isTappable {
+                tappableIndexes.append(UInt(i))
+                fragmentAttributes = [
+                    "\(i)": isTappable,
+                    NSUnderlineStyleAttributeName: NSUnderlineStyle.styleSingle.rawValue,
+                    NSUnderlineColorAttributeName: UIColor.gray7
+                ]
+            }
+            
+            let fragmentAttributedString = NSAttributedString(string: text, attributes: fragmentAttributes)
+            attributedString.append(fragmentAttributedString)
+        }
+        
+        if let attributes = attributes {
+            attributedString.addAttributes(attributes, range: NSRange(location: 0, length: attributedString.length))
+        }
+        
+        self.tappableIndexes = tappableIndexes
+        attributedText = attributedString
+    }
 }
