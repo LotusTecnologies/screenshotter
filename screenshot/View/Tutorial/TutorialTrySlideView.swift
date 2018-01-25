ba76a5f2b5338149f25430840661fd28081f2cae
Corey Werner <cnotethegr8@cnote.local>
2018-01-25 10:06:09 +0200

    included sync photos when skipping tutorial

--- screenshot/View/Tutorial/TutorialTrySlideView.swift
+++ screenshot/View/Tutorial/TutorialTrySlideView.swift
@@ -85,6 +85,7 @@ public class TutorialTrySlideView : HelperView {
     // MARK: Skip
     
     @objc func skipButtonAction() {
+        AssetSyncModel.sharedInstance.syncPhotos()
         self.delegate?.tutorialTrySlideViewDidSkip(self)
     }
 }
