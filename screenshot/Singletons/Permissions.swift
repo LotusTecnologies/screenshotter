b57e7f4571b1081afc04ba31e5ed9e7be9920912
Jacob Relkin <jacobrelkin@gmail.com>
2018-01-25 13:23:08 +0200

    Added comment explaining why we dispatch to the main queue in the pushStatus setter

--- screenshot/Singletons/Permissions.swift
+++ screenshot/Singletons/Permissions.swift
@@ -43,6 +43,12 @@ final class PermissionsManager : NSObject, CLLocationManagerDelegate {
                 properties = ["token": token.description]
             }
             
+            /*
+                 This is a required change due to the fact that we can enter into this pushStatus setter from any queue,
+                 and analytics 'identify' logic can only be deterministically executed on the main queue due to
+                 downstream dependencies such as Intercom depending on this constraint.
+             */
+            
             DispatchQueue.main.async {
                 AnalyticsTrackers.standard.track(name, properties: properties)
                 
