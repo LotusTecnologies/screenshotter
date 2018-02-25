03553d1575b55ca218ab5ed1e8b2f5d41f491e5f
Corey Werner <cnotethegr8@cnote.local>
2018-02-25 11:14:54 +0200

    fixed merge conflict

--- screenshot/View/Generic/Button/SegmentedDropDownButton.swift
+++ screenshot/View/Generic/Button/SegmentedDropDownButton.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2018 crazeapp. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class SegmentedDropDownItem : NSObject {
     var pickerItems: [String]
--- screenshot/View/Product/ProductViewController.swift
+++ screenshot/View/Product/ProductViewController.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2018 crazeapp. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class ProductViewController : BaseViewController {
     fileprivate let scrollView = UIScrollView()
