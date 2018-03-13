2aee316dd3c61ea965fce33ad286abdb93ba994c
Corey Werner <cnotethegr8@cnote.local>
2018-03-13 08:57:46 +0200

    cherrypicked files from shopping-cart branch

---
+++ screenshot/View/Generic/ViewControllerTransitioning/ViewControllerAnimatedTransitioning.swift
@@ -0,0 +1,26 @@
+//
+//  ViewControllerAnimatedTransitioning.swift
+//  screenshot
+//
+//  Created by Corey Werner on 3/11/18.
+//  Copyright © 2018 crazeapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerAnimatedTransitioning: NSObject, UIViewControllerAnimatedTransitioning {
+    let isPresenting: Bool
+    
+    init(presenting: Bool) {
+        isPresenting = presenting
+        super.init()
+    }
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return 0.5
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        
+    }
+}
