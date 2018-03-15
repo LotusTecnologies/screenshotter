ddd30572a57fea348818a8f0b3b4b10f22adc71e
Jonathan Rose <jonathanrose@Jon-Roses-MacBook-Pro.local>
2018-03-15 19:07:06 +0200

    change correct thing to 100

--- screenshot/Model/AssetSyncModel.swift
+++ screenshot/Model/AssetSyncModel.swift
@@ -701,7 +701,7 @@ class AssetSyncModel: NSObject {
         }
         let fetchOptions = PHFetchOptions()
         fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
-        fetchOptions.fetchLimit = 100
+        fetchOptions.fetchLimit = 1
         
         let assets = PHAsset.fetchAssets(withLocalIdentifiers: [assetId], options: fetchOptions)
         
@@ -801,7 +801,7 @@ class AssetSyncModel: NSObject {
         }
         fetchOptions.predicate = NSPredicate(format: "creationDate >= %@ AND (mediaSubtype & %d) != 0", installDate, PHAssetMediaSubtype.photoScreenshot.rawValue)
         fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
-        fetchOptions.fetchLimit = 25
+        fetchOptions.fetchLimit = 100
         futureScreenshotAssets = PHAsset.fetchAssets(with: .image, options: fetchOptions)
     }
     
