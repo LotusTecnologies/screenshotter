4aa141e3d6d44bdbea6eb37732a227eb5150b2b8
Jonathan Rose <rose@screenshopit.com>
2018-05-01 17:54:41 +0300

    add tracking data

--- screenshot/Singletons/Analytics.swift
+++ screenshot/Singletons/Analytics.swift
@@ -18,7 +18,24 @@ extension Bool {
         return self ? "true" : "false"
     }
 }
+
 class Analytics {
+    static private func addScreenshotProperitesFrom(trackingData:String?, toProperties:inout [String:Any]) {
+        do {
+            if let string = trackingData, let data = string.data(using: .utf8) {
+                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    json.forEach { (arg) in
+                        let (key, value) = arg
+                        if let key = key as? String {
+                            toProperties["screenshot-\(key)"] = value
+                        }
+                    }
+                }
+            }
+        }catch {
+            
+        }
+    }
     static func propertiesFor(_ matchstick:Matchstick) -> [String:Any] {
         var properties:[String:Any] = [:]
         
@@ -26,15 +43,8 @@ class Analytics {
             properties["screenshot-imageURL"] = uploadedImageURL
         }
         
-        do {
-            if let string = matchstick.trackingInfo, let data = string.data(using: .utf8) {
-                if let json = JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
-                    json.forEach { properties[$0] = $1 }
-                }
-            }
-        }catch {
-            
-        }
+        self.addScreenshotProperitesFrom(trackingData: matchstick.trackingInfo, toProperties: &properties)
+        
         return properties
     }
     
@@ -43,16 +53,7 @@ class Analytics {
         if let uploadedImageURL = screenshot.uploadedImageURL {
             properties["screenshot-imageURL"] = uploadedImageURL
         }
-        
-        do {
-            if let string = matchstick.trackingInfo, let data = string.data(using: .utf8) {
-                if let json = JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
-                    json.forEach { properties[$0] = $1 }
-                }
-            }
-        }catch {
-            
-        }
+        self.addScreenshotProperitesFrom(trackingData: screenshot.trackingInfo, toProperties: &properties)
         
         return properties
     }
