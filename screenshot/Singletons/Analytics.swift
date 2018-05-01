84e6de9892775a2d6cd8d129ea4c4472bf95652b
Jonathan Rose <rose@screenshopit.com>
2018-05-01 15:39:32 +0300

    disable debug ui

--- screenshot/Singletons/Analytics.swift
+++ screenshot/Singletons/Analytics.swift
@@ -107,19 +107,22 @@ class Analytics {
         }
     }
     static func debugShowLoggedAnalytics(eventName: String, properties: [AnyHashable:Any], destinations:[String]){
-        DispatchQueue.main.async {
-            if let viewController = AppDelegate.shared.window?.rootViewController {
-                let announcement = Announcement(title: eventName, subtitle: destinations.joined(separator: ", "), image: nil, duration:10.0, action:{
-                    //notification was tapped
-                    let alert = UIAlertController.init(title: eventName, message: String(describing: properties), preferredStyle: .alert)
-                    
-                    alert.addAction(UIAlertAction.init(title: "OK", style: .cancel, handler: nil))
-                    viewController.present(alert, animated: true, completion: nil)
-                    
-                })
-                Whisper.show(shout: announcement, to: viewController, completion: {
-                    print("The shout was silent.")
-                })
+        let showDebugUI = false
+        if showDebugUI {
+            DispatchQueue.main.async {
+                if let viewController = AppDelegate.shared.window?.rootViewController {
+                    let announcement = Announcement(title: eventName, subtitle: destinations.joined(separator: ", "), image: nil, duration:10.0, action:{
+                        //notification was tapped
+                        let alert = UIAlertController.init(title: eventName, message: String(describing: properties), preferredStyle: .alert)
+                        
+                        alert.addAction(UIAlertAction.init(title: "OK", style: .cancel, handler: nil))
+                        viewController.present(alert, animated: true, completion: nil)
+                        
+                    })
+                    Whisper.show(shout: announcement, to: viewController, completion: {
+                        print("The shout was silent.")
+                    })
+                }
             }
         }
     }
