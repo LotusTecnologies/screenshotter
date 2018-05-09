b75f823ba768859c6eebb7e61b8bd784ad41e35f
Corey Werner <cnotethegr8@cnote.local>
2018-05-09 09:13:26 +0300

    created gift card view

---
+++ screenshot/View/Cart/CartGiftCardView.swift
@@ -0,0 +1,23 @@
+//
+//  CartGiftCardView.swift
+//  screenshot
+//
+//  Created by Corey Werner on 5/9/18.
+//  Copyright © 2018 crazeapp. All rights reserved.
+//
+
+import UIKit
+
+class CartGiftCardView: UIView {
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .crazeGreen
+    }
+    
+    
+}
