89cea57867749c8e6213eff64d1089907ad77553
Jonathan Rose <rose@screenshopit.com>
2018-05-17 14:16:08 +0300

    fix issue where user can sumbit multiple times to discover while request is outgoing, fix issue where can't share to discover where source==screenshot. fix issue where no analytics were logged when shared from productsList page. fix issue where the page is not logged in the share event

--- screenshot/Model/DataModel.swift
+++ screenshot/Model/DataModel.swift
@@ -21,6 +21,10 @@ enum ScreenshotSource : String {
     case share
     case tutorial
     case nativeShare = "native-share" //Andriod only - here for completment of analytics
+    
+    var isUserGenerated:Bool {
+        return (self == .nativeShare || self == .gallery || self == .share || self == .unknown || self == .screenshot || self == .camera)
+    }
 }
 
 class DataModel: NSObject {
