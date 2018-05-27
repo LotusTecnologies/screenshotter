66c4d560d80850aa2f2d12416f73409b64a4bdf7
Jonathan Rose <rose@screenshopit.com>
2018-05-27 08:02:19 +0300

    share on favorite - move share code to maanger

---
+++ screenshot/View/Generic/ScreenshotShareManager.swift
@@ -0,0 +1,85 @@
+//
+//  ScreenshotShareManager.swift
+//  screenshot
+//
+//  Created by Jonathan Rose on 5/27/18.
+//  Copyright © 2018 crazeapp. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ScreenshotShareManager {
+    
+    static func share(screenshot:Screenshot, in viewController:UIViewController){
+    
+    }
+    static func share(screenshot:Screenshot, presentViewController:@escaping (_ vc:UIViewController)->(), dismissTarget:Any, dismissAction:Selector){
+        
+        let screenshotObjectId = screenshot.objectID
+        let alert = UIAlertController.init(title: "share_to_discover.action_sheet.title".localized, message: "share_to_discover.action_sheet.message".localized, preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction.init(title: "share_to_discover.action_sheet.discover".localized, style: .default, handler: { (a) in
+            if let screenshot = DataModel.sharedInstance.mainMoc().screenshotWith(objectId: screenshotObjectId) {
+                if !screenshot.canSubmitToDiscover {
+                    let alert = UIAlertController.init(title: nil, message: "share_to_discover.action_sheet.error.alread_shared".localized, preferredStyle: .alert)
+                    
+                    alert.addAction(UIAlertAction.init(title: "generic.ok".localized, style: .cancel, handler: { (a) in
+                        Analytics.trackShareIncomplete(screenshot: screenshot)
+                    }))
+                    presentViewController( alert )
+                }else {
+                    screenshot.submitToDiscover()
+                    let thankYou = ThankYouForSharingViewController()
+                    thankYou.closeButton.addTarget(dismissTarget, action: dismissAction, for: .touchUpInside)
+                    presentViewController( thankYou )
+                    Analytics.trackShareDiscover(screenshot: screenshot, page: .screenshotList)
+                }
+            }
+            
+        }))
+        alert.addAction(UIAlertAction.init(title: "share_to_discover.action_sheet.social".localized, style: .default, handler: { (a) in
+            if let screenshot = DataModel.sharedInstance.mainMoc().screenshotWith(objectId: screenshotObjectId) {
+
+                let introductoryText = "screenshots.share.title".localized
+                
+                let screenshotObjectId = screenshot.objectID
+                var items:[Any]? = nil
+                
+                // iOS 11.1 has a bug where copying to clipboard while sharing doesn't put a space between activity items.
+                let space = " "
+                
+                if let shareLink = screenshot.shareLink, let shareURL = URL.init(string: shareLink) {
+                    items = [introductoryText, space, shareURL]
+                }else{
+                    if let url = URL.init(string: "https://getscreenshop.com/") {
+                        let screenshotActivityItemProvider = ScreenshotActivityItemProvider.init(screenshot: screenshot, placeholderURL:url)
+                        items = [introductoryText, space, screenshotActivityItemProvider]
+                    }
+                }
+                if let items =  items {
+                    let activityViewController = UIActivityViewController.init(activityItems: items, applicationActivities: [])
+                    activityViewController.excludedActivityTypes = [UIActivityType.addToReadingList, UIActivityType.airDrop, UIActivityType.init("com.apple.reminders.RemindersEditorExtension"), UIActivityType.init("com.apple.mobilenotes.SharingExtension")]
+                    activityViewController.completionWithItemsHandler = { (activityType, completed, returnedItems, activityError) in
+                        let screenshot = DataModel.sharedInstance.mainMoc().screenshotWith(objectId: screenshotObjectId)
+                        if (completed) {
+                            Analytics.trackShareSocial(screenshot: screenshot)
+                        } else {
+                            Analytics.trackShareIncomplete(screenshot: screenshot)
+                        }
+                    }
+                    presentViewController(activityViewController)
+                }
+            }
+        }))
+        alert.addAction(UIAlertAction.init(title: "generic.cancel".localized, style: .cancel, handler: { (a) in
+            let screenshot = DataModel.sharedInstance.mainMoc().screenshotWith(objectId: screenshotObjectId)
+            Analytics.trackShareIncomplete(screenshot: screenshot)
+            
+        }))
+        presentViewController ( alert )
+        
+        Analytics.trackSharedScreenshotStarted(screenshot: screenshot)
+    }
+    
+}
