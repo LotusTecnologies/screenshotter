a39e2484d21455e1201569491971564a882c3b4e
Corey Werner <cnotethegr8@cnote.local>
2018-05-30 11:36:02 +0300

    organized files

---
+++ screenshot/View/Authorize/ResetPasswordViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ResetPasswordViewController.swift
+//  screenshot
+//
+//  Created by Corey Werner on 5/30/18.
+//  Copyright © 2018 crazeapp. All rights reserved.
+//
+
+import UIKit
+
+class ResetPasswordViewController: UIViewController {
+    
+}
