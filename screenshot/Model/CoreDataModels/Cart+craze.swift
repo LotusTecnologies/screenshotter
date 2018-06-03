47995a70aa68f4c31db83fd631f5f6463b2f81c6
Jonathan Rose <rose@screenshopit.com>
2018-06-03 08:26:58 +0300

    refactor data model to pull out frc, and extension into a different files

---
+++ screenshot/Model/CoreDataModels/Cart+craze.swift
@@ -0,0 +1,23 @@
+//
+//  Car+craze.swift
+//  screenshot
+//
+//  Created by Jonathan Rose on 6/3/18.
+//  Copyright © 2018 crazeapp. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension Cart {
+    var estimatedTax:Float {
+        get{
+            return 0.06 * ( self.subtotal + self.shippingTotal )
+        }
+    }
+    var estimatedTotalOrder:Float {
+        get{
+            return self.subtotal + self.shippingTotal + self.estimatedTax
+        }
+    }
+}
