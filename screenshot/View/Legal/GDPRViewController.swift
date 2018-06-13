3c90c268602f26880051f3473dd3345488c37b7b
Corey Werner <cnotethegr8@cnote.local>
2018-06-13 14:28:54 +0300

    building gdpr pages

---
+++ screenshot/View/Legal/GDPRViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GDPRViewController.swift
+//  screenshot
+//
+//  Created by Corey Werner on 6/13/18.
+//  Copyright © 2018 crazeapp. All rights reserved.
+//
+
+import UIKit
+
+class GDPRViewController: UIViewController {
+    enum Rows: Int {
+        case notification
+        case imageDetection
+    }
+}
