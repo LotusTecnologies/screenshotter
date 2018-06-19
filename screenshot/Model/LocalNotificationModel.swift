fb4f8e81cc9b5658bec6a04d3699641cd9910147
Gershon Kagan <gershon@screenshopit.com>
2018-06-19 14:43:29 +0300

    Start migrating local notifications to a model.

---
+++ screenshot/Model/LocalNotificationModel.swift
@@ -0,0 +1,13 @@
+//
+//  LocalNotificationModel.swift
+//  screenshot
+//
+//  Created by Gershon Kagan on 06/19/2018.
+//  Copyright © 2018 crazeapp. All rights reserved.
+//
+
+import UIKit
+
+class LocalNotificationModel {
+
+}
