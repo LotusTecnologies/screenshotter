e54d9acf4ee06e964fe2cd7fffbfd713d1ac1222
Jonathan Rose <rose@screenshopit.com>
2018-07-03 07:54:43 +0300

    download user data whenlogin from FB

--- screenshot/Controllers/UserAccountManager.swift
+++ screenshot/Controllers/UserAccountManager.swift
@@ -196,19 +196,25 @@ class UserAccountManager : NSObject {
                     if let user = self.user {
                         self.linkFirebaseStuffForFacebookFor(user: user)
                     }
-                    fulfil(.facebookOld)
+                    self.downloadAndReplaceUserData().always {
+                        fulfil(.facebookOld)
+                    }
+                    
                 }else{
                     reject(error)
                 }
             }else if let user = result?.user {
                self.linkFirebaseStuffForFacebookFor(user: user)
+                let downloadAndReplaceUserDataTask = self.downloadAndReplaceUserData()
                 self.databaseRef.child("users").child(user.uid).child("createdAt").observeSingleEvent(of: .value) { (snapshot) in
-                    if let _ = snapshot.value as? NSNumber {
-                        fulfil(.facebookOld)
-                    }else{
-                        let now = NSNumber.init(value: (Date().timeIntervalSince1970 as Double) )
-                        self.databaseRef.child("users").child(user.uid).child("createdAt").setValue(now)
-                        fulfil(.facebookNew)
+                    downloadAndReplaceUserDataTask.always {
+                        if let _ = snapshot.value as? NSNumber {
+                            fulfil(.facebookOld)
+                        }else{
+                            let now = NSNumber.init(value: (Date().timeIntervalSince1970 as Double) )
+                            self.databaseRef.child("users").child(user.uid).child("createdAt").setValue(now)
+                            fulfil(.facebookNew)
+                        }
                     }
                 }
             }else {
