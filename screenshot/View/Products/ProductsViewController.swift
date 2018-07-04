e5a0338c2213dda52fdb5af3e9f2c9f367f8d4f3
Gershon Kagan <gershon@screenshopit.com>
2018-07-04 22:51:04 +0300

    Fix devScreenshot scheme setting up prod pushwoosh. Accept favorited|tapped|disabled remote notifications.

--- screenshot/View/Products/ProductsViewController.swift
+++ screenshot/View/Products/ProductsViewController.swift
@@ -432,7 +432,7 @@ extension ProductsViewControllerCollectionView : UICollectionViewDelegateFlowLay
             let product = self.productAtIndex(indexPath.item)
             product.recordViewedProduct()
             self.recoverLostSaleManager.didClick(on: product)
-            LocalNotificationModel.shared.registerCrazePriceAlert(id: product.id, lastPrice: product.floatPrice, hasPriceAlerts: product.hasPriceAlerts)
+            LocalNotificationModel.shared.registerCrazeTappedPriceAlert(id: product.id, lastPrice: product.floatPrice)
             if let productViewController = presentProduct(product, atLocation: .products) {
                 productViewController.similarProducts = products
             }
@@ -466,9 +466,10 @@ extension ProductsViewControllerCollectionView : UICollectionViewDelegateFlowLay
         if isFavorited {
             let _ = ShoppingCartModel.shared.populateVariants(productOID: product.objectID)
             Analytics.trackProductFavorited(product: product, page: .productList)
-            LocalNotificationModel.shared.registerCrazePriceAlert(id: product.id, lastPrice: product.floatPrice, hasPriceAlerts: product.hasPriceAlerts)
+            LocalNotificationModel.shared.registerCrazeFavoritedPriceAlert(id: product.id, lastPrice: product.floatPrice)
         }else{
             Analytics.trackProductUnfavorited(product: product, page: .productList)
+            LocalNotificationModel.shared.deregisterCrazeFavoritedPriceAlert(id: product.id)
         }
     }
     
@@ -480,7 +481,7 @@ extension ProductsViewControllerCollectionView : UICollectionViewDelegateFlowLay
         
         let product = self.productAtIndex(indexPath.item)
         product.recordViewedProduct()
-        LocalNotificationModel.shared.registerCrazePriceAlert(id: product.id, lastPrice: product.floatPrice, hasPriceAlerts: product.hasPriceAlerts)
+        LocalNotificationModel.shared.registerCrazeTappedPriceAlert(id: product.id, lastPrice: product.floatPrice)
 
         if let cell = collectionView?.cellForItem(at: indexPath) as? ProductsCollectionViewCell {
             self.productCollectionViewManager.burrow(cell: cell, product: product, fromVC: self)
